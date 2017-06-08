9f381ba5005f97f3fba297c5ee08b3165733e74d
damaraju31 <damaraju.abhilash@gmail.com>
2017-06-09 01:52:42 +0530

    final commit

--- Assignment.py
+++ Assignment.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, make_response, jsonify
+from flask import Flask, render_template, request, make_response, jsonify, url_for, redirect
 from flask_pymongo import PyMongo
 
 app = Flask(__name__)
@@ -12,6 +12,22 @@ mongo = PyMongo(app)
 @app.errorhandler(404)
 def not_found(error):
     return make_response(jsonify({'error': 'Notfound'}), 404)
+
+
+@app.route('/success/<Pid>', methods=['POST'])
+def success(Pid):
+    result = request.form
+    categories = str(result['categories']).lower()
+    tags = str(result['tags']).lower().split(',')
+    price = int(result['price'])
+    name = str(result['nm'])
+    discount = int(result['discount'])
+    quantity = int(result['quantity'])
+    Id = int(Pid)
+    mongo.db.product.update({'SKU_id': Id}, {
+        '$set': {'name': name, "categories": categories, "tags": tags, "price": price, "discount": discount,
+                 "quantity": quantity}})
+    return redirect(url_for('product_list'))
 
 
 @app.route('/')
@@ -28,7 +44,8 @@ def products():
     if len(tags) == 1 and tags[0] == "":
         prd_list = mongo.db.product.find({"price": {'$lte': int(price)}, "categories": categories}, {'_id': 0})
     else:
-        prd_list = mongo.db.product.find({"price": {'$lte': int(price)}, "categories": categories, "tags": {'$in': tags}},{'_id': 0})
+        prd_list = mongo.db.product.find(
+            {"price": {'$lte': int(price)}, "categories": categories, "tags": {'$in': tags}}, {'_id': 0})
     return render_template('output.html', prd_list=prd_list)
 
 
@@ -38,6 +55,11 @@ def product_list():
     return render_template('result.html', product_list=product_list)
 
 
+@app.route('/admin/products/<item>')
+def update_product(item):
+    product_info = mongo.db.product.find({"SKU_id": int(item)})
+    return render_template('update.html', product_info=product_info)
+
+
 if __name__ == '__main__':
     app.run(debug=True)
-
